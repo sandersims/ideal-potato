31feead06dd31e8c2663678f32e0ed5fd0f2af34
sandersims <sander02@tlu.ee>
2023-09-27 16:27:56 +0300

    Working 27-09-2023

--- src/App.js
+++ src/App.js
@@ -4,44 +4,21 @@ import Toggle from "./components/Toggle"
 import Show from "./components/Show"
 import Maths from "./components/Maths"
 import MyForm from "./components/MyForm"
+import PropDrilling from "./components/PropDrilling"
+import Context from "./components/Context"
+import Fetching from "./components/Fetching"
 
 function App() {
   const [show, setShow] = useState(true)
 
   const toggleShow = () => setShow(previousShow => !previousShow)
 
-  return (import { useState } from "react"
-  import "./App.css"
-  import Toggle from "./components/Toggle"
-  import Show from "./components/Show"
-  import Maths from "./components/Maths"
-  import MyForm from "./components/MyForm"
-  import PropDrilling from "./components/PropDrilling"
-  import Context from "./components/Context"
-  import Fetching from "./components/Fetching"
-  
-  function App() {
-    const [show, setShow] = useState(true)
-  
-    const toggleShow = () => setShow(previousShow => !previousShow)
-  
-    return (
-      <div className="Container">
-        <h1>Raimo</h1>
-        <Fetching />
-        <PropDrilling />
-        <Context />
-        <MyForm />
-        <Maths />
-        <Show show={show} />
-        <Toggle toggleShow={toggleShow} />
-      </div>
-    )
-  }
-  
-  export default App
+  return (
     <div className="Container">
       <h1>Illar Kukeke</h1>
+      <Fetching />
+      <PropDrilling />
+      <Context />
       <MyForm />
       <Maths />
       <Show show={show} />
